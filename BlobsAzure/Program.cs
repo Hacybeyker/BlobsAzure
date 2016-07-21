28ab57aad2575dee577092cfdabb3fe6ec465c8e
Hacybeyker <Hacybeyker@hotmail.com>
2016-07-21 06:26:41 -0500

    Implementacion de Blobs en Azure para subir archivos, se implementaron los metodos: Upload, Download y Delete.

--- BlobsAzure/Program.cs
+++ BlobsAzure/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+/* https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs */
+/* http://www.genbetadev.com/programacion-en-la-nube/operaciones-de-fichero-contra-un-blob-en-windows-azure */
+/* Install-Package WindowsAzure.Storage -Version 7.1.2 */
+/* Install-Package Microsoft.WindowsAzure.ConfigurationManager */
 
 namespace BlobsAzure
 {
@@ -10,6 +15,41 @@ namespace BlobsAzure
     {
         static void Main(string[] args)
         {
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("facturacionelectronica");
+                CloudBlockBlob blockBlob;
+                #region Upload
+                blockBlob = container.GetBlockBlobReference("20600995805-01-F001-00000001");
+                using (var fileStream = File.OpenRead(@"C:\FacturacionElectronicaSNET\Factura\enviado\20600995805-01-F001-00000001.xml"))
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+                Console.WriteLine("Subida Exitosa");
+                #endregion
+                #region Download
+                blockBlob = container.GetBlockBlobReference("20600995805-01-F001-00000001");
+                using (var fileStream = File.OpenWrite(@"C:\Users\Carlos\Downloads\20600995805-01-F001-00000001"))
+                {
+                    blockBlob.DownloadToStream(fileStream);
+                }
+                Console.WriteLine("Descarga Exitosa");
+                //Console.ReadLine();
+                #endregion
+                #region Delete
+                blockBlob = container.GetBlockBlobReference("20600995805-01-F001-00000001");
+                //blockBlob.Delete();
+                Console.WriteLine("Eliminacion Exitosa");
+                #endregion
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
         }
     }
 }
